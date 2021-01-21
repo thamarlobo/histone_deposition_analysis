817e1147448765c5a8e5f8807e7e05fa951b23e3
thamarlobo <58851010+thamarlobo@users.noreply.github.com>
2021-01-21 18:05:17 +0100

    Update bootstrap_bias_gcquantiles.pl

--- Bootstrap_analysis/bootstrap_bias_gcquantiles.pl
+++ Bootstrap_analysis/bootstrap_bias_gcquantiles.pl
@@ -4,8 +4,8 @@ use warnings;
 use Parallel::ForkManager;
 use POSIX ();
 
-# Script bootstraps *_results.txt outputfiles of map_histonseq_around_oris_gc.pl 
-# Input: _results.txt outputfile(s) of map_histonseq_around_oris_gc.pl
+# Script bootstraps *_results.txt outputfiles of map_seqdata_around_oris_gc.pl 
+# Input: _results.txt outputfile(s) of map_seqdata_around_oris_gc.pl
 # Output: Outputfile(s) *_bootstrapped.txt contains the summed number of fragments from the forward strand and reverse strand per bin per GC quantile for each bootstrapround
 # Outputformat: six tab-separated columns: $boot (=bootstrap round), $b (=bin), $n_f (=summed number of fragments from forward strand), $n_r (=summed number of fragments from reverse strand), $frvalue (=frvalue, not used downstream), $q(=GC quantile)
 # Outputfile *_bootstrapped.txt can be used to determine the 95 % Confidence Interval per group of origins (categorized based on GC percentage) by:
@@ -16,7 +16,7 @@ use POSIX ();
 # Author: Thamar Jessurun Lobo
 
 # Paths and parameters 
-my @files = ("PATH(S)_TO_RESULTS.TXT"); # Insert path(s) to *_results.txt (map_histonseq_around_oris_gc.pl)
+my @files = ("PATH(S)_TO_RESULTS.TXT"); # Insert path(s) to *_results.txt (map_seqdata_around_oris_gc.pl)
 my $n_rounds = 1000; # Number of bootstrap rounds. Default is 1000
 
 # Get parallel sessions
